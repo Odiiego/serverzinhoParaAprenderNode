506ffac169b459a7c801e7740548635a11835310
Odiiego <odiegopaula@gmail.com>
2023-10-21 04:09:47 -0300

    Terminei! xD

--- server.js
+++ server.js
@@ -9,10 +9,35 @@ class Emitter extends EventEmitter {}
 
 const myEmitter = new Emitter();
 
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
+
 const PORT = process.env.PORT || 3500;
+
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const rawData = await fsPromises.readFile(
+      filePath,
+      !contentType.includes('image') ? 'utf8' : '',
+    );
+    const data =
+      contentType === 'application/json' ? JSON.parse(rawData) : rawData;
+    response.writeHead(filePath.includes('404.html') ? 404 : 200, {
+      'Content-Type': contentType,
+    });
+    response.end(
+      contentType === 'application/json' ? JSON.stringify(data) : data,
+    );
+  } catch (err) {
+    console.log(err);
+    myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
+    response.statusCode = 500;
+    response.end();
+  }
+};
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+  myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
 
   const extension = path.extname(req.url);
   let contentType;
@@ -48,9 +73,26 @@ const server = http.createServer((req, res) => {
       : contentType === 'text/html'
       ? path.join(__dirname, 'views', req.url)
       : path.join(__dirname, req.url);
+  if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+  const fileExists = fs.existsSync(filePath);
+
+  if (fileExists) {
+    serveFile(filePath, contentType, res);
+  } else {
+    switch (path.parse(filePath).base) {
+      case 'old-page.html':
+        res.writeHead(301, { Location: '/new-page.html' });
+        res.end();
+        break;
+      case 'www-page.html':
+        res.writeHead(301, { Location: '/' });
+        res.end();
+        break;
+      default:
+        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+    }
+  }
 });
 
 server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-
-// myEmitter.on('log', (msg) => logEvents(msg));
-// myEmitter.emit('log', 'Log event emitted!');
