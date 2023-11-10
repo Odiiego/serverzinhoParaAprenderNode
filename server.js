27317e49ee712293b2599f7d6a8c73557b328e90
Odiiego <odiegopaula@gmail.com>
2023-11-10 04:28:37 -0300

    Lidando com rotas no Express.

--- server.js
+++ server.js
@@ -28,34 +28,11 @@ app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-  // res.sendFile('./views/index.html', { root: __dirname });
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-  res.redirect(301, 'new-page.html');
-});
-
-app.get(
-  '/hello(.html)?',
-  (req, res, next) => {
-    console.log('aff');
-    next();
-  },
-  (req, res) => {
-    res.send('Hello World!');
-  },
-);
-
-app.get('/*', (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
   res.status(404);
